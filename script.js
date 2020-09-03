10e182ce3a5b5a8f152b1fb221fd813333bf51ea
Larry X <papadocarock@gmail.com>
2020-09-03 16:11:03 -0400

    first push

---
+++ script.js
@@ -0,0 +1,15 @@
+var symbols = ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", ".", ];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerCased = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ];
+var upperCased = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ];
+var generateBtn = document.getElementById("generate");
+var password = [];
+
+
+generateBtn.addEventListener("click", yourFuncForUserInput());
+
+function yourFuncForUserInput() {
+
+    for (i = 0; i < generateBtn.length; i++) {
+        var password = password = lower.lowerCased(Math.floor(Math.random() * Mathfloor(value.length - 1)
+                }
